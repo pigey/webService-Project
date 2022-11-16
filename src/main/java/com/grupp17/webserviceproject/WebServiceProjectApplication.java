2da39dd3ab316626ba31b7c256fd5875e42980b5
Filip Mathsson <pigfof@gmail.com>
2022-11-16 09:25:09 +0100

    Initial commit

---
+++ src/main/java/com/grupp17/webserviceproject/WebServiceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.grupp17.webserviceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupp17/webserviceproject/WebServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp17.webserviceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
