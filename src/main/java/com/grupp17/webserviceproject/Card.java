f1de2397cbea6651016ac42b09bc616eb7eb3327
Sofia <sofia.montgomery@gmail.com>
2022-11-28 15:22:39 +0100

    added lombok annotations to Card-class

--- src/main/java/com/grupp17/webserviceproject/Card.java
+++ src/main/java/com/grupp17/webserviceproject/Card.java
@@ -1,8 +1,17 @@
 package com.grupp17.webserviceproject;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 @Entity
 @Table(name = "Card")
 public class Card {
@@ -21,51 +30,10 @@ public class Card {
     @Column(name = "description")
     private String description;
 
-    public  Card(){}
     public Card(String firstName, String lastName, int age, String description) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.description = description;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
