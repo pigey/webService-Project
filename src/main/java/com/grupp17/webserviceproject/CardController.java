de3278f04553609e5f7927c1dc463f83baace59d
Sofia <sofia.montgomery@gmail.com>
2022-11-28 10:13:39 +0100

    added postmapping

--- src/main/java/com/grupp17/webserviceproject/CardController.java
+++ src/main/java/com/grupp17/webserviceproject/CardController.java
@@ -1,10 +1,26 @@
 package com.grupp17.webserviceproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class CardController{
+public class CardController {
+    private final CardRepository cardRepository;
+    @Autowired
+    public CardController(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+    @PostMapping("/cards")
+    public ResponseEntity<Card> createCard(@RequestBody final Card card){
+        try {
+            Card _card = cardRepository.save(card);
 
-
+            return new ResponseEntity<>(_card, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
